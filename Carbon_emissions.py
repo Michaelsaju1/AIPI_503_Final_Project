4bba1a419775479a296998903f86b9e415c63ec0
Michaelsaju1 <153621782+Michaelsaju1@users.noreply.github.com>
2025-08-21 20:52:18 -0400

    Create Carbon_emissions.py
    Add overarching thought process. Change as required.

---
+++ Carbon_emissions.py
@@ -0,0 +1,23 @@
+API_KEY = os.environ["API_KEY_HERE"]
+
+# Step 1: Read in data
+df = read.csv('')
+
+# Step 2: Create data pipeline
+def data_pipeline(dataframe)
+  return df_validated
+
+# Step 3: Get Updated dataframe with carbon footprint after API query
+def carbon_footprint(dataframe):
+  return df_updated
+
+# Step 4: Put everything on Streamlit
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+st.header("")
+st.image(
+    "image link",
+    caption="caption",
+    use_container_width=True
+)
